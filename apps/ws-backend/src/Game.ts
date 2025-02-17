ad484b4ab9a3e6555cffdd972ba818503c045ecf
Aniket Mahajan <mahajananiket207@gmail.com>
2025-02-17 23:15:57 +0530

    create make move function

--- apps/ws-backend/src/Game.ts
+++ apps/ws-backend/src/Game.ts
@@ -1,5 +1,6 @@
-import { Chess } from "chess.js";
+import { Chess, Move } from "chess.js";
 import { randomUUID } from "crypto";
+import { Users } from "./SocketManager";
 
 
 export class Game {
@@ -8,11 +9,28 @@ export class Game {
     public player2UserId: string | null;
     public chessBoard: Chess;
 
-    constructor(player1UserId: string, player2UserId: string, gameId: string){
+    constructor(player1UserId: string, player2UserId: string | null, gameId?: string){
         this.player1UserId = player1UserId;
         this.player2UserId = player2UserId;
         this.gameId = gameId ?? randomUUID();
         this.chessBoard = new Chess();
-        console.log("Game is initialize");
+    }
+
+    makeMove(
+        user: Users,
+        move: Move
+    ){
+        if(this.chessBoard.turn() === "w" && user.userId !== this.player1UserId){
+            return;
+        }
+
+        if(this.chessBoard.turn() === "b" && user.userId !== this.player2UserId){
+            return;
+        }
+
+        this.chessBoard.move({
+            from: move.from,
+            to: move.to
+        })
     }
 }
--- apps/ws-backend/src/GameManager.ts
+++ apps/ws-backend/src/GameManager.ts
@@ -14,11 +14,39 @@ export class GameManager{
 
     addUser(user: Users) {
         this.users.push(user);
-        console.log(user.id + user.name + user.socket + user.userId);
         this.addHandler(user);
     }
 
-    private addHandler(user: Users){
+    removeUser(socket: WebSocket){
+        // const user = this.users.find( x => user?.socket === socket);
+        // if(!user){
+        //     console.log("user not found");
+        //     return;
+        // }
 
+        this.users = this.users.filter((user) => user.socket !== socket);
+
+        //implement a function to remove socket of user
+        // implement a function to remove game of this socket 
+    }
+
+    private addHandler(user: Users){
+        user.socket.on("message", (data) => {
+            const message = JSON.parse(data.toString());
+            if(message.type === "INIT_GAME"){
+                const game = new Game(user.userId, null);
+            }
+
+            if(message.type === "MOVE"){
+                const gameId = message.payload.gameId();
+                const game = this.game.filter((game) => gameId === game.gameId);
+                if(game){
+                    game.makeMove(user, message.payload.move)
+                }
+            }
+
+
+
+        })
     }
 }
