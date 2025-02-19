2f823eaeef5cd67a28ac9146b833abc66adeb809
Aniket Mahajan <mahajananiket207@gmail.com>
2025-02-19 15:30:32 +0530

    Complete socket manager class

--- apps/ws-backend/src/SocketManager.ts
+++ apps/ws-backend/src/SocketManager.ts
@@ -56,7 +56,30 @@ class SocketManager {
         });
     }
 
-    removeUser(){
-        
+    removeUser(user: Users){
+        const roomId = this.userRoomMapping.get(user.userId);
+        if(!roomId){
+            console.error("User not interesting in any room?");
+            return;
+        }
+
+        const room = this.interestedSockets.get(roomId) || [];
+        const remainingUsers = room.filter(u => {
+            u.userId !== user.userId
+        });
+
+        this.interestedSockets.set(
+            roomId,
+            remainingUsers
+        )
+
+        if(this.interestedSockets.get(roomId)?.length === 0){
+            this.interestedSockets.delete(roomId);
+        }
+
+        this.userRoomMapping.delete(user.userId);
+
     }
 }
+
+export const socketManager = SocketManager.getInstance();
