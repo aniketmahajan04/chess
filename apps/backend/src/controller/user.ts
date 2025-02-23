b90649c5aa05729d02a74bed67c7df22f99456df
Aniket Mahajan <mahajananiket207@gmail.com>
2025-02-23 22:27:05 +0530

    create signup router

--- apps/backend/src/controller/user.ts
+++ apps/backend/src/controller/user.ts
@@ -1,7 +1,32 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import { db } from "../db";
+
 
 const signup = async (req: Request, res: Response): Promise<void> => {
     const { name, email, password } = req.body;
+
+    try{
+        const hashedPassword = await bcrypt.hash(password, 5);
+
+        const user = await db.user.create({
+            data: {
+                name: name,
+                email: email,
+                password: hashedPassword
+            }
+        });
+
+        res.status(200).json({
+            msg: "signup successfull!"
+        })
+        
+    }catch(error) {
+        console.error("Something went wrong ", error);
+        res.status(500).json({
+            msg: "Error while creating user"
+        })
+    }
 }
 
 export {
